da24cafdd20febcb74d65a6cd7b734e1536fa3e8
yakisoba0101 <aippei4318@gmail.com>
2026-09-24 23:43:06 +0900

    first Commit.

---
+++ bmi.js
@@ -0,0 +1,15 @@
+// 体重：68kg
+const weight = 68;
+
+// 身長：1.7m
+const height = 1.7;
+
+//BMI
+let bmi; 
+
+// 計算式：[体重(kg)]÷[身長(m)×身長(m)]
+bmi = weight / (height * height);
+
+
+//出力
+console.log(bmi);
